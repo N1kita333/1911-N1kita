d2824e4c85ed9e6b7b3474f0dbad5983106d4aad
1911_N <batavata1@gmail.com>
2023-02-05 13:44:00 +0200

    lesson4.1 || dz3

--- lesson3/lesson3.py
+++ lesson3/lesson3.py
@@ -4,14 +4,22 @@ import random
 class Student:
     def __init__(self, name):
         self.name = name
-        self.progress = 0
-        self.gladness = 100
+        self.progress = 0.5
+        self.gladness = 50
         self.alive = True
+        self.money = 0.4
 
     def to_study(self):
         print("Я навчаюсь")
         self.progress += 0.1
         self.gladness -= 6
+        self.money -= 0.1
+
+    def to_cash_balance(self):
+        print("Я роблю")
+        self.money += 0.4
+        self.gladness -= 10
+        self.progress -= 0.1
 
     def to_sleep(self):
         print("я пішов спати")
@@ -21,6 +29,7 @@ class Student:
         print("я пішов розважатися")
         self.progress -= 0.1
         self.gladness += 5
+        self.money -= 0.2
 
     def is_alive(self):
         if self.progress < 0:
@@ -32,20 +41,27 @@ class Student:
         if self.gladness < 0:
             print('Все пропало. В мене депрессія')
             self.alive = False
+        if self.money < 0:
+            print('Все пропало. Мене вигнали з МКА, тепер я бомж')
+            self.alive = False
+
     def end_off_day(self):
         print(f'задоволеність - {self.gladness}')
         print(f'прогрес       - {round(self.progress, 2)}')
+        print(f'гроші       - {round(self.money, 2)}')
 
     def live(self, day):
         info = f'День{day} з життя {self.name}'
         print(f'{info:=^40}')
-        choice = random.randint(1, 3)
+        choice = random.randint(1, 4)
         if choice == 1:
             self.to_study()
         elif choice == 2:
             self.to_sleep()
         elif choice == 3:
             self.to_have_fun()
+        elif choice == 4:
+            self.to_cash_balance()
 
         self.end_off_day()
         self.is_alive()
