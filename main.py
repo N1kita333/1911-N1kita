d8eab7b5602e7142fec33c5cb3b5307e0fd08856
1911_N <batavata1@gmail.com>
2023-02-12 18:04:09 +0200

    dz4

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 class House:
-    def __init__(self):
-        self.food = 0
-        self.mess = 0
+    def __init__(self, kitchen):
+        self.kitchen = kitchen
+        self.located = []
+
+    def add_located(self, *animal):
+        for located in animal:
+            self.located.append(located)
+
+    def print_located(self):
+        if self.located == []:
+            print('Кухня зараз порожня')
+        else:
+            print(F'Зараз в {self.kitchen} знаходиться:')
+            for located in self.located:
+                print(located.name)
 
 
 class Animal:
@@ -14,8 +26,8 @@ class Animal:
         Animal.count_of_animal += 1
         print(f"Привіт, я з'явився, я {self.name}")
 
-    def grow(self, years=0.1):
-        self.years += 0.1
+    #def grow(self, years=0.1):
+    #    self.years += 0.1
 
     #def __str__(self):
     #    return f'я {self.name}, мій зріст {self.years} '
@@ -24,11 +36,17 @@ class Animal:
     #    print(f'Привіт, я {self.name}, {self.description}, мені {self.years} і я пішов гуляти')
 
 
-Tom = Animal(name='Tom', years=2, description='я кот')
+#Tom = Animal(name='Tom', years=2, description='я кот')
 
 
 
-Ashley = Animal(name='Ashley', years=1, description='я кіт')
+#Ashley = Animal(name='Ashley', years=1, description='я кіт')
 
 # Ashley.grow()
 print(Animal.count_of_animal, "шт.")
+Ashley = Animal("Ashley",  years=2, description='я кот')
+Tom = Animal("Tom", years=1, description='я кіт')
+house = House("кухні")
+house.print_located()
+house.add_located(Ashley, Tom)
+house.print_located()
