9a88b3d859566ca7b3ac62462e07e3a4c848d0e6
1911_N <batavata1@gmail.com>
2023-02-26 14:59:19 +0200

    dz7

---
+++ lesson 7/dz7.py
@@ -0,0 +1,7 @@
+import colorama
+from colorama import Fore
+
+print(Fore.RED + "Ім'я - Микита")
+print(Fore.BLUE + "Прізвище - Жереги")
+print(Fore.YELLOW + "Вік - 14 років")
+print(Fore.WHITE + "Група шагу - 1911")
